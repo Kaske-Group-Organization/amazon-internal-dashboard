961294e402931fee28d8328099ce52300a9ba452
ep-sai <206729836+ep-sai@users.noreply.github.com>
2026-05-27 12:52:56 +0200

    Update main.jsx

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { PublicClientApplication } from '@azure/msal-browser'
+import { MsalProvider } from '@azure/msal-react'
+import { msalConfig } from './auth/msalConfig.js'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const msalInstance = new PublicClientApplication(msalConfig)
+
+msalInstance.initialize().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <MsalProvider instance={msalInstance}>
+        <App />
+      </MsalProvider>
+    </React.StrictMode>
+  )
+})
